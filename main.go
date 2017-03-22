051e4e56c0f49b00fb92ac0c22bf4fb5078537f8
Toru Furukawa <torufurukawa@gmail.com>
2017-03-22 16:26:00 +0900

    Add creating client

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import "github.com/strava/go.strava"
+import "os"
 
 func main() {
-	_ = strava.NewClient
+	client := strava.NewClient(os.Getenv("STRAVA_ACCESS_TOKEN"))
+	_ = client
 }
